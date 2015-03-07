3151312bc58251d7bf6e1a622ad5788fd51ef18a
Dane Hammer <dane.molotok@gmail.com>
2015-03-07 15:53:27 -0600

    make separate pages

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
 
 get '/' do
-  erb :index, :locals => {:log_content => File.read('/usr/share/chef-client/client.log')}
+  erb :index
 end
 
-get '/resume.txt' do
-  redirect 'https://www.dropbox.com/s/kw9l0ji56miy90e/resume.txt?raw=1'
+get '/this_site' do
+  erb :this_site, :locals => {:log_content => File.read('/usr/share/chef-client/client.log')}
+end
+
+get '/resume' do
+  erb :resume
 end
