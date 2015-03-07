302413523f715d98b69057f777a850b06cdd7f24
Dane Hammer <dane.molotok@gmail.com>
2015-03-07 16:33:58 -0600

    add /resume.FORMAT routes

--- app.rb
+++ app.rb
@@ -11,3 +11,15 @@ end
 get '/resume' do
   erb :resume
 end
+
+get '/resume.txt' do
+  redirect 'https://www.dropbox.com/s/kw9l0ji56miy90e/resume.txt?dl=1'
+end
+
+get '/resume.pdf' do
+  redirect 'https://www.dropbox.com/s/5eplgwdv863uvxk/resume.pdf?dl=1'
+end
+
+get '/resume.docx' do
+  redirect 'https://www.dropbox.com/s/nku3qn48oylbk85/resume.docx?dl=1'
+end
